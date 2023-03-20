867a65234f69d4d66764ff8a23f7d9448b253df1
hiroshi egami <doegadoega@hotmail.com>
2023-03-20 10:43:25 +0900

    the first commit

---
+++ components/img/components/vision_innovation.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h5>ほげほげ</h5>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+
+const articles = ref<{ id: number, title: string }[]>([]);
+
+articles.value = [{
+  id: 1,
+  title: "Nuxt3入門",
+}, {
+  id: 2,
+  title: "Jest再入門",
+}];
+</script>
+
+<template>
+  <div>
+	<header>
+		<div class="flex">
+			<label id="effect_title">
+				<h3>School Memories</h3>
+			</label>
+		</div>
+		<div class="right_">
+			<h1>メモリアル映像制作</h1>
+		</div>		
+	</header>
+    <div class="container">
+		<div>
+			<img src="@/assets/stock_474365622.jpg" 
+				alt="Icon"
+				class="mr-3" />
+
+		</div>
+    </div>
+    <footer>
+      © 2022 mamezou-tech
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+header {
+  padding: 1rem;
+}
+footer {
+  margin-top: 2rem;
+  background-color: #8080ee;
+  padding: 1rem;
+}
+.container {
+  margin: 2rem;
+}
+
+.flex{
+    display: flex;
+    justify-content: space-between;
+}
+.flex>p{
+    width: 49%;
+}
+
+.right_ {
+	text-align: right;
+	margin: 0 auto;
+	font-weight: bold;
+	padding-top: 10px;
+	padding-bottom: 5px;
+	font-size: small;
+}
+</style>
+
